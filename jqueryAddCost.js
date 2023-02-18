2682cf4c62ef1524ba4dce569e1c260afe43958d
Mister Coder <3dprintingwebstienl@gmail.com>
2023-02-18 12:00:12 +0100

    compress function in haskell

--- jqueryAddCost.js
+++ jqueryAddCost.js
@@ -1,43 +1,33 @@
-jQuery(document).ready(function ($) {
-  if ($(".simple-product-price .woocommerce-Price-amount").length) {
-    var html = $(".woocommerce-product-details__short-description").html();
+jQuery(document).ready(function ($)
+{
+  if ($(".simple-product-price .woocommerce-Price-amount").length) 
+  {
+    const html = $(".woocommerce-product-details__short-description").html();
     $("input.qty").bind("change keyup input", function () {
-      var pepQty = $(this).val();
-      var pepPrice = $(
-        ".simple-product-price .woocommerce-Price-amount"
-      ).html();
-      var pepPrice = pepPrice.split("</span>");
-      var pepPrice = pepPrice[1].replace(/,/g, ".");
-      let pepTotalPrice = pepQty * parseFloat(pepPrice);
-      var pepTotalPrice1 = pepTotalPrice.toFixed(2);
-      var pepTotalPrice2 = pepTotalPrice1.replace(".", ",");
-      var pepTotalPriceFormatted =
-        "<span style='font-size: 28px;'>Totaalprijs: €" +
-        pepTotalPrice2 +
-        "</span>";
-      $(".woocommerce-product-details__short-description").html(
-        html + pepTotalPriceFormatted
-      );
+      const pepQty = $(this).val();
+      const randomName = $( ".simple-product-price .woocommerce-Price-amount").html();
+      const randomName1 = randomName.split("</span>");
+      const randomName2 = randomName1[1].replace(/,/g, ".");
+      let pepTotalPrice = pepQty * parseFloat(randomName2);
+      const pepTotalPrice1 = pepTotalPrice.toFixed(2);
+      const pepTotalPrice2 = pepTotalPrice1.replace(".", ",");
+      const pepTotalPriceFormatted = "<span style='font-size: 28px;'>Totaalprijs: €" + pepTotalPrice2 + "</span>";
+      $(".woocommerce-product-details__short-description").html( html + pepTotalPriceFormatted );
     });
-  } else {
+  } 
+  else 
+  {
     var html = $(".woocommerce-product-details__short-description").html();
     $("input.qty").bind("change keyup input", function () {
       var pepQty = $(this).val();
-      var pepPrice = $(
-        ".woocommerce-variation-price .woocommerce-Price-amount"
-      ).html();
+      var pepPrice = $(".woocommerce-variation-price .woocommerce-Price-amount").html();
       var pepPrice = pepPrice.split("</span>");
       var pepPrice = pepPrice[1].replace(/,/g, ".");
       let pepTotalPrice = pepQty * parseFloat(pepPrice);
       var pepTotalPrice1 = pepTotalPrice.toFixed(2);
       var pepTotalPrice2 = pepTotalPrice1.replace(".", ",");
-      var pepTotalPriceFormatted =
-        "<span style='font-size: 28px;'>Totaalprijs: €" +
-        pepTotalPrice2 +
-        "</span>";
-      $(".woocommerce-product-details__short-description").html(
-        html + pepTotalPriceFormatted
-      );
+      var pepTotalPriceFormatted = "<span style='font-size: 28px;'>Totaalprijs: €" + pepTotalPrice2 + "</span>";
+      $(".woocommerce-product-details__short-description").html(html + pepTotalPriceFormatted);
     });
   }
 });
