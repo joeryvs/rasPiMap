cd5912038798ce7c20409d8004812fc742123d75
mintLaptop <joery@van_Santen.nl>
2024-01-02 12:18:24 +0100

    used html parsing to get the image urls

--- mapPython/webscrape.py
+++ mapPython/webscrape.py
@@ -2,9 +2,43 @@ import urllib.request
 import urllib.response
 import itertools
 import pathlib
+from bs4 import BeautifulSoup
 
 def web_url(n:int) -> str :
-    return f"{n}"
+    return f"https://xkcd.com/{n}/"
+
+
+def getImageList_Lower(): 
+    for x in itertools.count(1):
+        my_url = web_url(x)
+        img = comicUrl(my_url)
+        if img is None:
+            return 
+        yield img
+
+def getImageList_Upper():
+    for x in itertools.count(405):
+        my_url = web_url(x)
+        img = comicUrl(my_url)
+        if img is None:
+            return 
+        yield img
+
+def saveImageList(file):
+    with open(file=file,mode="w") as f:
+        for line in getImageList():
+            print(line,file=f)
+
+def comicUrl(url):
+    with urllib.request.urlopen(url=url) as response:
+        if 200 <= response.status < 300: 
+            textt = response.read().decode("utf-8")
+        else:
+            return None
+    soup = BeautifulSoup(textt,features="html.parser")
+    outer = soup.find("div",id = "comic")
+    imgAttr = outer.findChild("img")
+    return imgAttr["src"]
 
 def saveToFile(pic,n:int) -> None:
     filepath = pathlib.Path("myFiles",f"pic{n}.png")
@@ -19,13 +53,14 @@ def requestData(url):
 
 def ravenScrape():
     url2 = "https://imgs.xkcd.com/comics/the_raven.jpg"
+    url2 = "https:" + "//imgs.xkcd.com/comics/convincing_pickup_line.png"
     with urllib.request.urlopen(url2) as response:
         if response is None :
             return
         if response.status != 200:
             print ("no file")
             return 
-        with open("raven_1.jpg","wb") as f:
+        with open("raven_3.jpg","wb") as f:
             f.write(response.read())
 def scrapeloop():
     for x in range(1000):
@@ -46,7 +81,8 @@ def scrapeloop():
 
 def main():
     try:
-        scrapeloop()
+        # saveImageList(pathlib.Path("comics.txt"))
+        ravenScrape()
     finally:
         pass
 
