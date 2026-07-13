d8782b65c201c34bff269eb6d042ce9cac396516
joeryvs <joeryvansanten@kpnmail.nl>
2026-07-13 21:33:47 +0200

    Utility script for scraping user which takes 2 parameters

---
+++ deviant/gallary-pages/scrape_user.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+# 2 arguments, arg 1 is the username, arg 2 is the amount of pages to scrape
+if [[ $# != 2 ]] ; then
+    echo "$0 USER AMOUNT"
+    exit 1;
+fi
+user="$1"
+amount="$2"
+
+# check first argument in a valid user
+re='^[a-z]+$'
+if ! [[ $user =~ $re ]] ; then
+    echo "error: not a valid user" >&2;
+    exit 1;
+fi
+
+# check second argument is a number
+re='^[0-9]+$'
+if ! [[ $amount =~ $re ]] ; then
+   echo "error: Not a number" >&2;
+   exit 1
+fi
+
+url="https://www.deviantart.com/$user/gallery?page=[1-$amount]"
+dir="$(dirname $0)/$user"
+# create directory
+mkdir -p "$dir"
+# download all pages
+curl "$url" -o "$dir/gallery_page_#1.html"
